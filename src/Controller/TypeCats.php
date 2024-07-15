3d7e84e67d88e65d952640507188aea6139808fd
Ka-vo <k.sagaydachnaya@webpractik.ru>
2024-07-15 09:18:03 +0300

    init: initialization commit

---
+++ src/Controller/TypeCats.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+//use Symfony\Component\Routing\Attribute\Route;
+
+class TypeCats extends AbstractController
+{
+    #[Route('/home', name: 'HomePage', methods: ['GET', 'HEAD'])]
+    public function cat(): Response
+    {
+        return $this->render('pets/home.html.twig');
+    }
+
+    #[Route('/dogAndCats/{param}', name: 'CatDogPage', methods: ['GET', 'HEAD'])]
+    public function dogAndCats($param): Response
+    {
+        return $this->render('pets/show.html.twig', [
+            'cat' => 'Информация о домашних животных - '. $param
+
+        ]);
+    }
+}
